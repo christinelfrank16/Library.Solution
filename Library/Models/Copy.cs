dc7e53eb0499b2e4644367d6b29f7ae0cd81b1dd
Christine Frank and Rachael Bouissey <christine+rachael@>
2019-10-23 14:38:45 -0700

    redirect user to transaction history

--- Library/Models/Copy.cs
+++ Library/Models/Copy.cs
@@ -4,13 +4,8 @@ namespace Library.Models
     {
         public int CopyId{get;set;}
         public int BookId{get;set;}
-        public bool ToBeCheckedOut { get; set; }
         public int CheckoutId { get; set; }
         public Book Book { get; set; }
-        public Copy()
-        {
-            ToBeCheckedOut = false;
-        }
 
     }
 }
